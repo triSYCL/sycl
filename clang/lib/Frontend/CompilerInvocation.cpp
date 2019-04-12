043298698802050077a9e6e0911a397b2d1eb058
Andrew Gozillon <andrew.gozillon@yahoo.com>
2019-04-12 15:48:37 -0700

    Reverting forced no exceptions for now as it breaks host try/catch compilation
    As the device still compiles all the host code, it will encounter host 
    side try + catches and throw errors. The problem needs a better solution 
    unfortunately, this small change is too good to be true sadly.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2841,8 +2841,7 @@ static void ParseLangArgs(LangOptions &Opts, ArgList &Args, InputKind IK,
 
   // Set the flag to prevent the implementation from emitting device exception
   // handling code for those requiring so.
-  if ((Opts.OpenMPIsDevice && T.isNVPTX()) || Opts.OpenCLCPlusPlus ||
-       Opts.SYCLIsDevice) {
+  if ((Opts.OpenMPIsDevice && T.isNVPTX()) || Opts.OpenCLCPlusPlus) {
     Opts.Exceptions = 0;
     Opts.CXXExceptions = 0;
   }
