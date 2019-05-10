4aecec871b901e37260079ff1090b5783a811953
Andrew Gozillon <andrew.gozillon@yahoo.com>
2019-05-10 10:55:41 -0700

    [SYCL][LLVM Pass][sycl-xocc] Adding Default DDR Bank Assignment When Compiling
    KernelPropGen no longer generates a .txt file it generates a bash shell 
    file for sycl-xocc to invoke that injects useful environment information 
    (in the form of environment variables) for the script to use (Names and 
    default DDR bank assignments for now).
    
    sycl-xocc now invokes the generated bash script that contains these 
    environment variables containing kernel names and default DDR Bank 
    assignment arguments and performs compilation on a per kernel basis and 
    passes the DDR bank assignments to the appropriate linker stage.
    
    This script data injection concept can be leveraged in the future to 
    easily add things like SLR bank assignment for CUs and generating of 
    more CUs based on arguments. No need to create complex bash logic, just 
    have the pass do the heavy lifting in C++.

--- llvm/lib/SYCL/KernelPropGen/KernelPropGen.cpp
+++ llvm/lib/SYCL/KernelPropGen/KernelPropGen.cpp
@@ -59,9 +59,9 @@ struct KernelPropGen : public ModulePass {
   bool runOnModule(Module &M) override {
     SmallString<256> TDir;
     llvm::sys::path::system_temp_directory(true, TDir);
-    std::string file = "KernelNames_" + M.getSourceFileName();
+    std::string file = "KernelProperties_" + M.getSourceFileName();
     llvm::sys::path::append(TDir, file);
-    llvm::sys::path::replace_extension(TDir, "txt",
+    llvm::sys::path::replace_extension(TDir, "sh",
       llvm::sys::path::Style::native);
 
     llvm::raw_fd_ostream O(GetWriteStreamID(TDir.str()),
@@ -70,20 +70,65 @@ struct KernelPropGen : public ModulePass {
     if (O.has_error())
       return false;
   
-    // 1) The option of using Accessors is ONLY viable if I can get the 
-    //  integration header inside llvm passes, I need to see if I can 
-    //  snatch it 
-    // 2) The other option is going to be to assume that arguments to kernels 
-    //  with an address space on them are always buffers and to assign them to 
-    //  DDR banks.
-    // i) How does xocc decide that it needs to infer the specific arguments it's
-    //  passed to DDR banks?
-    for (auto &F : M.functions()) {
+    // Generate a bash script to inject environment information containing 
+    // kernel properties that we need so we can compile. This script is called
+    // from sycl-xocc. #!/bin/bash
+    O << "#!/bin/bash\n";
+   
+    std::string kernelNames = "";
+    std::string DDRArgs = "";
+    for (auto &F : M.functions()) {     
       if (isKernel(F)) {
-        O << F.getName() << "\n";
+        kernelNames += (" \"" + F.getName() + "\" ").str();
+        
+        for (auto& Arg : F.args()) {
+          if (Arg.getType()->isPointerTy())
+          // if the argument is a pointer in the global (1) or constant (3) 
+          // address space it should be assigned to an explicit default DDR 
+          // Bank of 0 to prevent assignment to DDR banks that are not 0.
+          // This is to prevent mismatches between the SYCL runtime when 
+          // declaring OpenCL buffers and the precompiled kernel, XRT will 
+          // error out if there is a misamtch. Only OpenCL global memory is 
+          // assigned to a DDR bank, this includes constant as it's just 
+          // read-only global memory. 
+          // \todo When adding an explicit way for users to specify DDR banks 
+          // from the SYCL runtime this should be modified as well as the buffer 
+          // XRT extensions.
+          if (Arg.getType()->isPointerTy()
+           && (Arg.getType()->getPointerAddressSpace() == 1 
+           || Arg.getType()->getPointerAddressSpace() == 3)) {
+              // This currently forces a default assignment of DDR banks to 0 
+              // as some platforms have different Default DDR banks and buffers 
+              // default to DDR Bank 0. Perhaps it is possible to query the 
+              // specific platform and reassign the buffers to different default 
+              // DDR banks based on the platform. But this would require a 
+              // change for every new platform. In either case, this puts in 
+              // infrastructure to assign DDR banks at compile time for a CU 
+              // if the information is passed down.
+              // This: Assigns a Default 0 DDR bank to all initial compute 
+              // unit's, the _1 postfix to the kernel name represents the 
+              // default compute unit name. If more than one CU is generated 
+              // (which we don't support yet in any case) then they would be 
+              // KernelName_2..KernelName_3 etc.
+              DDRArgs += ("--sp " + F.getName() + "_1." + Arg.getName() 
+                          + ":DDR[0] ").str();
+          }
+        }
+        
+        O << "\n"; // line break for new set of kernel properties
       }
     }
-
+    
+    // output our list of kernel names as a bash array we can iterate over
+    if (!kernelNames.empty())
+       O << "declare -a KERNEL_NAME_ARRAY=(" << kernelNames << ")\n";
+    
+    // output our --sp args containing DDR assignments.
+    // Should look something like: --sp kernelName_1.arg_0:DDR[0] --sp ...
+    if (!DDRArgs.empty())
+       O << "DDR_BANK_ARGS=\"" << DDRArgs << "\"\n"; 
+      
+       
     // The module probably changed
     return true;
   }
