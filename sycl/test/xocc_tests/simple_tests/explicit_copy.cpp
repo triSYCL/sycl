54c0da768da795c8adced74e9cd81783bbbb1382
Andrew Gozillon <andrew.gozillon@yahoo.com>
2019-06-05 20:33:42 -0700

    [SYCL][Test] Removing unrequired code from edge_detection.cpp

--- sycl/test/xocc_tests/simple_tests/explicit_copy.cpp
+++ sycl/test/xocc_tests/simple_tests/explicit_copy.cpp
@@ -3,6 +3,27 @@
 /*
   An example based on the SYCL Specifications explicitcopy.cpp example, tests
   that the Handlers copy method is working as intended.
+
+  There is a much more complex example testing all the variations of copy and
+  fill named: handler_mem_op.cpp, most of the use cases in the test work except
+  the 2D/3D Accessor copy tests, which XRT throws the following errors for:
+
+  [XRT] ERROR: src_origin,region,src_row_pitch,src_slice_pitch out of range
+  [XRT] ERROR: src_origin,region,src_row_pitch,src_slice_pitch out of range
+  [XRT] ERROR: src_origin,region,src_row_pitch,src_slice_pitch out of range
+  OpenCL API failed. /storage/ogozillo/intel-sycl/sycl/sycl/source/detail/
+    memory_manager.cpp:282: OpenCL API returns: -30 (CL_INVALID_VALUE)
+  [XRT] ERROR: src_origin,region,src_row_pitch,src_slice_pitch out of range
+  [XRT] ERROR: Internal error. cl_mem doesn't map to buffer object
+  [XRT] ERROR: Internal error. cl_mem doesn't map to buffer object
+  [XRT] ERROR: Internal error. cl_mem doesn't map to buffer object
+  OpenCL API failed. /storage/ogozillo/intel-sycl/sycl/sycl/source/detail/
+    memory_manager.cpp:243: OpenCL API returns: -6 (CL_OUT_OF_HOST_MEMORY)
+  [XRT] ERROR: Internal error. cl_mem doesn't map to buffer object
+  terminate called after throwing an instance of 'cl::sycl::runtime_error'
+  free(): corrupted unsorted chunks
+
+  \TODO fix the 2D/3D Copy for XRT
 */
 
 #include <numeric>
