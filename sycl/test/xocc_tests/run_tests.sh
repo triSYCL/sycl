745fd52605e5d3ef2ec201b2864c5a95ac941287
Andrew Gozillon <andrew.gozillon@yahoo.com>
2019-07-12 16:54:50 -0700

    [SYCL][Test] Add Intel/HW compilation to run_tests.sh, minor QOL fixes

--- sycl/test/xocc_tests/run_tests.sh
+++ sycl/test/xocc_tests/run_tests.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 # A small convenience script for compiling and running all the tests for FPGA
 # sw_emu and hw_emu. As it gets a little tiring and time consuming manually
-# doing it. This script is likely to take a very longtime to finish.
+# doing it. This script is likely to take a very long time to finish if you run
+# everything. Upwards of a few hours.
+#
+# The most important tests to run and make sure still function when merging
+# upstream are:
+#   id_mangle
+#   math_mangle
+#   parallel_for_ND_range
+#   reqd_work_group_size
+#   single_task_vector_add
+#   edge_detection
+#   internal_defines
+#
+# These test are the "main" functionality that could break on upstream merge,
+# most of the rest is icing on the cake tests that are related to testing SYCL
+# functionality. They're also important, but larger breaking issues are more
+# likely to show in one of the above tests.
+#
+# Most of the tests will assert and stop the shell running right now. These
+# could perhaps be changed to something like a throw that's caught and prints an
+# error message.. which would perhaps be more user friendly.
 #
 # This script won't compare the results or give you feedback on the end result
 # of the tests. It will spit the output into a file which you'll unfortunately
@@ -48,25 +68,55 @@ fi
 
 CLANG_BIN=$1
 DEFAULT_XFPGA_ARGS=(-std=c++2a -fsycl -fsycl-targets=fpga64-xilinx-unknown-sycldevice -I$XILINX_XRT/include/ -lOpenCL)
+# this is just to make sure we didn't regress our own tests, you should also
+# run make check-all or make check-sycl from the build directory:
+# check-all - if there was any compiler modifications required
+# check-sycl - if it was just some SYCL runtime modifications
+DEFAULT_INTEL_ARGS=(-std=c++2a -fsycl -lOpenCL)
 
 # $1 Test File Name
 # $2 The tests directory
 # $3 XCL_EMULATION_MODE
-# $4 Extra Args for Compilation
-# $5 Extra Args for Execution
+# $4 Extra Args for Compilation, supplied as a ref to array
+# $5 Extra Args for Execution, supplied as a ref to array
 run_test () {
   export XCL_EMULATION_MODE=$3
+
+  USED_DEFAULT_ARGS=("${DEFAULT_XFPGA_ARGS[@]}")
+  if [ $3 = "intel" ]; then
+    USED_DEFAULT_ARGS=("${DEFAULT_INTEL_ARGS[@]}")
+  fi
+
+  # get reference to an array, even if it's 1 argument, it should be passed in
+  # as an array to make life simpler and avoid a lot of irritating logic
+  if [ ! -z "$4" ]; then
+    local -n COMPILATION_ARG_ARR_REF=$4
+  else
+    local COMPILATION_ARG_ARR_REF=""
+  fi
+
+  if [ ! -z "$5" ]; then
+    local -n RUNTIME_ARG_ARR_REF=$5
+  else
+    local RUNTIME_ARG_ARR_REF=""
+  fi
 
   echo "" >>  $TEST_OUTPUT_FILE
   echo "" >>  $TEST_OUTPUT_FILE
   echo "Compiling $2/$1.cpp" >>  $TEST_OUTPUT_FILE
-  $CLANG_BIN/clang++ "${DEFAULT_XFPGA_ARGS[@]}" `pkg-config --libs opencv` "$4" "$2/$1.cpp" \
-    -o "$2/$1.$XCL_EMULATION_MODE" >> $TEST_OUTPUT_FILE 2>&1
+  $CLANG_BIN/clang++ "${USED_DEFAULT_ARGS[@]}" "${COMPILATION_ARG_ARR_REF[@]}" \
+    "$2/$1.cpp" -o "$2/$1.$XCL_EMULATION_MODE" >> $TEST_OUTPUT_FILE 2>&1
 
-  echo "" >>  $TEST_OUTPUT_FILE
-  echo "" >>  $TEST_OUTPUT_FILE
-  echo "Executing $2/$1.cpp" >> $TEST_OUTPUT_FILE
-  ./"$2/$1.$XCL_EMULATION_MODE" "$5" >> $TEST_OUTPUT_FILE 2>&1
+  # The default is not to run for HW, to run tests for hw remove the if block,
+  # unset XCL_EMULATION_MODE and it should make an attempt to execute the test
+  # although you will of course require the correct piece of hardware
+  if [[ $3 != "hw" ]]; then
+    echo "" >>  $TEST_OUTPUT_FILE
+    echo "" >>  $TEST_OUTPUT_FILE
+    echo "Executing $2/$1.cpp" >> $TEST_OUTPUT_FILE
+    ./"$2/$1.$XCL_EMULATION_MODE" "${RUNTIME_ARG_ARR_REF[@]}" \
+      >> $TEST_OUTPUT_FILE 2>&1
+  fi
 
   echo "" >>  $TEST_OUTPUT_FILE
   echo "" >>  $TEST_OUTPUT_FILE
@@ -86,16 +136,33 @@ test_list () {
   run_test "multi_parallel_for_ND_range" "simple_tests" "$1"
   run_test "parallel_for_ND_range" "simple_tests" "$1"
   run_test "reqd_work_group_size" "simple_tests" "$1"
+#  Note: There appears to be a race condition in hw_emu for
+#  single_task_vector_add, sometimes passes sometimes fails.
   run_test "single_task_vector_add" "simple_tests" "$1"
   run_test "vector_math" "simple_tests" "$1"
 
   emconfigutil -f $XILINX_PLATFORM --od sdaccel_ports/vision/edge_detection
 
+  # This test is a bit of a monster for time consumption when run in hw_emu,
+  # if it compiles and runs the first few iterations...it's a success
+  COMPILER_ARG_ARR=(`pkg-config --libs opencv`)
+  RUNTIME_ARG_ARR=(sdaccel_ports/vision/edge_detection/data/input/eiffel.bmp)
   run_test "edge_detection" "sdaccel_ports/vision/edge_detection" "$1" \
-    "" "sdaccel_ports/vision/edge_detection/data/input/eiffel.bmp"
+    COMPILER_ARG_ARR RUNTIME_ARG_ARR
 }
+
+# compile and test for intel, don't want to break existing functionality.
+test_list "intel"
 
 # Compile and Run Tests for Software and Hardware Emulation
 test_list "sw_emu"
 test_list "hw_emu"
+# I would advise only running this on a subset of the tests or if you have a
+# weekend to run the tests.
+# The sycl-xocc script doesn't play well with multiple parallel invocations of
+# the compiler due to the way it treats temporary files right now. At the very
+# least it's not been rigorously tested. Currently the script is improved via
+# lazy evolution, if its required it's added on..  This script also does not
+# execute the tests, it will just compile them.
+# test_list "hw"
 
