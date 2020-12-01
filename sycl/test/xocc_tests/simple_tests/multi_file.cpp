e6be9a0df646fba18268b1864704a9fb0e3f3be4
Gauthier Harnisch <tyker1@outlook.com>
2020-12-01 15:26:38 +0100

    [SYCL][XOCC] Add multi-file and split compilation support.

---
+++ sycl/test/xocc_tests/simple_tests/multi_file.cpp
@@ -0,0 +1,37 @@
+
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -DFILE1 -c %s -o %s1.o
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -c %s -o %s2.o
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s1.o %s2.o -o %t.out
+// RUN: %ACC_RUN_PLACEHOLDER %t.out
+
+#ifdef FILE1
+
+#include <CL/sycl.hpp>
+
+void test();
+
+int main() {
+  test();
+  cl::sycl::buffer<cl::sycl::cl_int, 1> Buffer(4);
+  cl::sycl::queue Queue{sycl::accelerator_selector{}};
+  cl::sycl::range<1> NumOfWorkItems{Buffer.get_count()};
+  Queue.submit([&](cl::sycl::handler &cgh) {
+    sycl::accessor Accessor(Buffer, cgh, sycl::write_only);
+    cgh.single_task<class Test1>([=] { Accessor[0] = 0; });
+  });
+}
+#else
+
+#include <CL/sycl.hpp>
+
+void test() {
+  cl::sycl::buffer<cl::sycl::cl_int, 1> Buffer(4);
+  cl::sycl::queue Queue{sycl::accelerator_selector{}};
+  cl::sycl::range<1> NumOfWorkItems{Buffer.get_count()};
+  Queue.submit([&](cl::sycl::handler &cgh) {
+    sycl::accessor Accessor(Buffer, cgh, sycl::write_only);
+    cgh.single_task<class Test2>(
+        [=] { Accessor[0] = 0; });
+  });
+}
+#endif
