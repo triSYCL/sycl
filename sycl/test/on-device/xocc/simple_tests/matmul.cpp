06c2eeeeb9051a306e7853dfd627e1758348d256
Gauthier Harnisch <gauthier@xilinx.com>
2021-03-23 02:00:25 -0700

    fix formating in RUN line of tests.

--- sycl/test/on-device/xocc/simple_tests/matmul.cpp
+++ sycl/test/on-device/xocc/simple_tests/matmul.cpp
@@ -1,7 +1,6 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 
-// RUN: env SYCL_DISABLE_PARALLEL_FOR_RANGE_ROUNDING=1 %ACC_RUN_PLACEHOLDER
-// %t.out
+// RUN: env SYCL_DISABLE_PARALLEL_FOR_RANGE_ROUNDING=1 %ACC_RUN_PLACEHOLDER %t.out
 
 #include <CL/sycl.hpp>
 
