74afa4724ae966d3597e1c8649e6ffcdf9defdc6
Gauthier Harnisch <gauthier@xilinx.com>
2021-04-15 02:00:14 -0700

    Add comments

--- sycl/tools/acap-intrinsic/acap-intrinsic.h
+++ sycl/tools/acap-intrinsic/acap-intrinsic.h
@@ -1,11 +1,22 @@
-
-#ifdef __SYCL_ACAP_INTRINSIC_H__
-#define __SYCL_ACAP_INTRINSIC_H__
+/// __SYCL_DEVICE_ONLY__ means we are on device code in sycl
+/// __ACAP_RT__ means we are compilling the device library
+/// neither __SYCL_DEVICE_ONLY__ nor __ACAP_RT__ means we are compilling for the
+/// host.
 
 #if defined(__SYCL_DEVICE_ONLY__) || defined(__ACAP_RT__)
+/// When compilling for device the definition is in the library
 #define DECL_POSTFIX ;
+#if defined(__SYCL_DEVICE_ONLY__)
+/// When compilling for sycl we need to mark function external or the compiler
+/// will complain about undefiend function.
+#define DECL_PREFIX SYCL_EXTERNAL
+#else
+#define DECL_PREFIX
+#endif
 #else
 #include <cassert>
+/// when compiling for the host the functions need to compile because they will
+/// be compiled but they shouldn't be executed.
 #define DECL_POSTFIX                                                           \
   {                                                                            \
     bool b = false;                                                            \
@@ -13,11 +24,8 @@
     assert(b && "this should only be called on device");                       \
     __builtin_unreachable();                                                   \
   }
-#endif
-
-#ifdef __SYCL_DEVICE_ONLY__
-#define DECL_PREFIX SYCL_EXTERNAL
-#else
+/// When compilling for the host functions need to be inline because they may
+/// colide
 #define DECL_PREFIX inline
 #endif
 
@@ -26,5 +34,3 @@ namespace acap_intr {
 DECL_PREFIX int get_coreid(void) DECL_POSTFIX
 
 }
-
-#endif
