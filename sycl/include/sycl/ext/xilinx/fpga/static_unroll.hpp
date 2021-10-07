45949bd9cc187139f88d6276288173341d65a60e
Luc Forget <lucf@xilinx.com>
2021-10-07 05:19:26 -0700

    Rename parallel_invoke to static_unroll

--- sycl/include/sycl/ext/xilinx/fpga/static_unroll.hpp
+++ sycl/include/sycl/ext/xilinx/fpga/static_unroll.hpp
@@ -1,4 +1,4 @@
-//==- parallel_invoke.hpp --- SYCL Xilinx loop unrolling       -------==//
+//==- parallel_invoke.hpp --- SYCL templated loop unrolling       -------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -17,6 +17,8 @@ namespace sycl {
 
 namespace ext::xilinx {
 namespace detail {
+
+    
 template <int firstStep, int firstOutStep, int inc>
 void inline normalized_parallel_invoke(auto &loop_step, auto &&loop_condition,
                                        int first, int bound) {
