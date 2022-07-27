3a0da77c69b46966d9b5ba476d5b49e8f730f54c
Tyker <tyker1@outlook.com>
2022-07-27 09:31:01 -0700

    [ArchGen] Add mlir-opt and mlir-lsp-server tools built with support for our dialects

---
+++ clang/tools/archgen-mlir/tools/archgen-lsp-server/archgen-lsp-server.cpp
@@ -0,0 +1,21 @@
+//===- archgen-lsp-server ---------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/InitAllDialects.h"
+#include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
+
+#include "archgen/Aprox/Aprox.h"
+#include "archgen/FixedPt/FixedPt.h"
+
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  registry.insert<archgen::aprox::AproxDialect>();
+  registry.insert<archgen::fixedpt::FixedPtDialect>();
+  return mlir::failed(mlir::MlirLspServerMain(argc, argv, registry));
+}
---
+++ clang/tools/archgen-mlir/tools/archgen-opt/archgen-opt.cpp
@@ -0,0 +1,36 @@
+//===- archgen-opt.cpp ------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+#include "archgen/Aprox/Aprox.h"
+#include "archgen/FixedPt/FixedPt.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllPasses();
+
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  registry.insert<archgen::aprox::AproxDialect>();
+  registry.insert<archgen::fixedpt::FixedPtDialect>();
+
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "ArchGen opt tool\n", registry));
+}
