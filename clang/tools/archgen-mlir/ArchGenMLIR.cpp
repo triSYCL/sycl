f3c68dfdc2ef92a09c49032e51c937492b0dd774
Tyker <tyker1@outlook.com>
2022-07-26 17:53:45 -0700

    [ArchGen] Add MLIR Generator infra

--- clang/tools/archgen-mlir/ArchGenMLIR.cpp
+++ clang/tools/archgen-mlir/ArchGenMLIR.cpp
@@ -12,14 +12,20 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/IR/Module.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include "mlir/IR/MLIRContext.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/MLIRContext.h"
 
+#include "clang/../../lib/CodeGen/CodeGenModule.h"
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/Attr.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/CodeGen/CodeGenAction.h"
 #include "clang/CodeGen/ModuleBuilder.h"
 #include "clang/Frontend/CompilerInstance.h"
@@ -29,26 +35,158 @@
 
 namespace {
 
-class ArchGenMLIRConsumer : public clang::ASTConsumer {
-  clang::CompilerInstance &Instance;
-  std::unique_ptr<clang::ASTConsumer> LLVMIRASTConsumer;
-  llvm::Module *LLVMModule;
-  llvm::LLVMContext *LLVMCtx;
-  mlir::MLIRContext ctx;
-
+class MLIRGenState {
   void InitMLIR() {
     /// Make sure that MLIR libs are properly linked with
     ctx.disableMultithreading();
-    ctx.loadDialect<mlir::LLVM::LLVMDialect>();
+    ctx.loadDialect<mlir::LLVM::LLVMDialect, mlir::arith::ArithmeticDialect,
+                    mlir::func::FuncDialect, archgen::aprox::AproxDialect,
+                    archgen::fixedpt::FixedPtDialect>();
+
+    builder.setInsertionPointToStart(module->getBody());
   }
 
 public:
-  ArchGenMLIRConsumer(clang::CompilerInstance &Instance,
-                      std::unique_ptr<clang::ASTConsumer> Consumer,
-                      llvm::Module *Module)
-      : Instance(Instance), LLVMIRASTConsumer(std::move(Consumer)),
-        LLVMModule(Module), LLVMCtx(&Module->getContext()) {
+  mlir::MLIRContext ctx;
+  mlir::OpBuilder builder;
+  mlir::Location loc;
+  mlir::OwningOpRef<mlir::ModuleOp> module;
+  clang::SourceManager &SM;
+  clang::CodeGen::CodeGenModule CGM;
+  llvm::DenseMap<clang::FunctionDecl *, mlir::func::FuncOp> FunctionMap;
+
+  MLIRGenState(clang::CompilerInstance &CI, llvm::Module *LLVMModule)
+      : builder(&ctx), loc(builder.getUnknownLoc()),
+        module(mlir::ModuleOp::create(loc)), SM(CI.getSourceManager()),
+        CGM(CI.getASTContext(),
+            CI.getPreprocessor().getHeaderSearchInfo().getHeaderSearchOpts(),
+            CI.getPreprocessor().getPreprocessorOpts(), CI.getCodeGenOpts(),
+            *LLVMModule, CI.getPreprocessor().getDiagnostics()) {
     InitMLIR();
+  }
+  mlir::Location getMLIRLocation(clang::SourceLocation loc) {
+    auto spellingLoc = SM.getSpellingLoc(loc);
+    auto lineNumber = SM.getSpellingLineNumber(spellingLoc);
+    auto colNumber = SM.getSpellingColumnNumber(spellingLoc);
+    auto fileId = SM.getFilename(spellingLoc);
+
+    auto ctx = module->getContext();
+    return mlir::FileLineColLoc::get(ctx, fileId, lineNumber, colNumber);
+  }
+
+  mlir::Type getMLIRType(clang::QualType Cqtype) {
+    auto *Ctype = Cqtype->getUnqualifiedDesugaredType();
+    if (auto *BT = llvm::dyn_cast<clang::BuiltinType>(Ctype)) {
+      if (Ctype->isBooleanType())
+        return builder.getIntegerType(8);
+      llvm::Type *T = CGM.getTypes().ConvertType(Cqtype);
+      if (T->isVoidTy())
+        return builder.getNoneType();
+      if (T->isFloatTy())
+        return builder.getF32Type();
+      if (T->isDoubleTy())
+        return builder.getF64Type();
+      if (T->isX86_FP80Ty())
+        return builder.getF80Type();
+      if (T->isFP128Ty())
+        return builder.getF128Type();
+      if (auto IT = llvm::dyn_cast<llvm::IntegerType>(T))
+        return builder.getIntegerType(IT->getBitWidth());
+    }
+
+    if (auto *FT = llvm::dyn_cast<clang::FunctionProtoType>(Ctype)) {
+      llvm::SmallVector<mlir::Type> Args;
+      llvm::transform(FT->param_types(), std::back_inserter(Args),
+                      [&](clang::QualType Elem) { return getMLIRType(Elem); });
+      return mlir::FunctionType::get(&ctx, Args,
+                                     {getMLIRType(FT->getReturnType())});
+    }
+
+    Cqtype->dump();
+    llvm_unreachable("unhandeled type");
+  }
+
+  mlir::func::FuncOp GetOrCreateFunc(clang::FunctionDecl *FD) {
+    auto &MLIRFunc = FunctionMap[FD];
+    if (MLIRFunc)
+      return MLIRFunc;
+
+    MLIRFunc = builder.create<mlir::func::FuncOp>(
+        loc, CGM.getMangledName(FD).str(),
+        getMLIRType(FD->getType()).cast<mlir::FunctionType>());
+    return MLIRFunc;
+  }
+};
+
+struct MLIREmiter : public clang::StmtVisitor<MLIREmiter, mlir::Value> {
+  using Base = clang::StmtVisitor<MLIREmiter, mlir::Value>;
+  MLIRGenState &state;
+  MLIREmiter(MLIRGenState &s) : state(s) {}
+  void Emit(clang::FunctionDecl* FD, mlir::func::FuncOp MLIRFunc) {
+    mlir::Block* block = MLIRFunc.addEntryBlock();
+    state.builder.setInsertionPointToStart(block);
+    Visit(FD->getBody());
+  }
+  mlir::Value VisitCompoundStmt(clang::CompoundStmt* CS) {
+    for (auto* Elem : CS->body()) {
+      Visit(Elem);
+    }
+    return {};
+  }
+  mlir::Value VisitCallExpr(clang::CallExpr* CE) {
+    CE->dumpColor();
+    return {};
+  }
+  mlir::Value VisitReturnStmt(clang::ReturnStmt *RS) {
+    mlir::Value val = Visit(RS->getRetValue());
+    state.builder.create<mlir::func::ReturnOp>(
+        state.getMLIRLocation(RS->getReturnLoc()), val.getType(), val);
+    return {};
+  }
+  mlir::Value VisitExpr(clang::Stmt *S) {
+    S->dump();
+    llvm_unreachable("unhandled Stmt");
+  }
+};
+
+class ArchGenMLIRConsumer : public clang::ASTConsumer {
+  clang::CompilerInstance &CI;
+  std::unique_ptr<clang::ASTConsumer> LLVMIRASTConsumer;
+  llvm::Module *LLVMModule;
+  llvm::LLVMContext *LLVMCtx;
+  MLIRGenState state;
+  MLIREmiter Visitor;
+
+public:
+  ArchGenMLIRConsumer(clang::CompilerInstance &CI,
+                      std::unique_ptr<clang::ASTConsumer> Consumer,
+                      llvm::Module *LLVMModule)
+      : CI(CI), LLVMIRASTConsumer(std::move(Consumer)), LLVMModule(LLVMModule),
+        LLVMCtx(&LLVMModule->getContext()), state(CI, LLVMModule),
+        Visitor(state) {}
+
+  std::deque<clang::FunctionDecl *> FuncToEmit;
+
+  bool isOurAnnotationAttr(clang::AnnotateAttr *Attr) {
+    return Attr->getAnnotation().startswith("archgen_mlir");
+  }
+
+  void addToEmit(clang::FunctionDecl* FD) {
+    LLVM_DEBUG(FD->dump(llvm::dbgs()));
+    state.GetOrCreateFunc(FD);
+    FuncToEmit.push_back(FD);
+  }
+
+  void FillFunctionBody(clang::FunctionDecl* FD) {
+    mlir::func::FuncOp MLIRFunc = state.GetOrCreateFunc(FD);
+    Visitor.Emit(FD, MLIRFunc);
+  }
+
+  void runEmit() {
+    for (auto* FD : FuncToEmit) {
+      if (FD->getBody())
+        FillFunctionBody(FD);
+    }
   }
 
   // Below is the exposed API of clang::ASTConsumer most place just need to
@@ -61,9 +199,17 @@ public:
     return LLVMIRASTConsumer->HandleCXXStaticMemberVarInstantiation(VD);
   }
   bool HandleTopLevelDecl(clang::DeclGroupRef D) override {
-    llvm::errs() << "---------------------------\n";
-    for (auto Elem : D)
-      Elem->dumpColor();
+    llvm::SmallVector<clang::Decl *> NewDG;
+    NewDG.reserve(std::distance(D.begin(), D.end()));
+    for (auto Elem : D) {
+      if (auto *FD = llvm::dyn_cast<clang::FunctionDecl>(Elem))
+        if (auto *AA = FD->getAttr<clang::AnnotateAttr>())
+          if (isOurAnnotationAttr(AA)) {
+            addToEmit(FD);
+            continue;
+          }
+      NewDG.push_back(Elem);
+    }
     return LLVMIRASTConsumer->HandleTopLevelDecl(D);
   }
   void HandleInlineFunctionDefinition(clang::FunctionDecl *D) override {
@@ -73,6 +219,8 @@ public:
     return LLVMIRASTConsumer->HandleInterestingDecl(D);
   }
   void HandleTranslationUnit(clang::ASTContext &Ctx) override {
+    runEmit();
+    state.module->dump();
     return LLVMIRASTConsumer->HandleTranslationUnit(Ctx);
   }
   void HandleTagDeclDefinition(clang::TagDecl *D) override {
