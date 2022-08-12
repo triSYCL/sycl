b4671ec30d8ca6480ba8760e666c90a6f56ba4fe
Ronan Keryell <rkeryell@xilinx.com>
2022-08-12 09:29:59 -0700

    Move Vitis IP tests to Alveo U200 xcu200-fsgd2104-2-e part
    Use a probably installed part since this one is installed by default
    with Vitis.

--- sycl/test/vitis/simple_tests/vitis_ip_export.cpp
+++ sycl/test/vitis/simple_tests/vitis_ip_export.cpp
@@ -1,10 +1,10 @@
 // REQUIRES: vitis && !vitis_cpu
 
-// RUN: %clangxx -std=c++20 --target=vitis_ip-xilinx %s -o %t.zip --vitis-ip-part=xc7vx330t-ffg1157-1 -### 2>&1 | FileCheck %s
-// RUN: %clangxx -std=c++20 --target=vitis_ip-xilinx %s -o %t.zip --vitis-ip-part=xc7vx330t-ffg1157-1
+// RUN: %clangxx -std=c++20 --target=vitis_ip-xilinx %s -o %t.zip --vitis-ip-part=xcu200-fsgd2104-2-e -### 2>&1 | FileCheck %s
+// RUN: %clangxx -std=c++20 --target=vitis_ip-xilinx %s -o %t.zip --vitis-ip-part=xcu200-fsgd2104-2-e
 
 // CHECK: clang-{{.*}}"-cc1" "-triple" "vitis_ip-xilinx" "-O3" "-disable-llvm-passes" {{.*}}
-// CHECK-NEXT: sycl_vxx.py" "ipexport" "--clang_path" {{.*}} "--target" "xc7vx330t-ffg1157-1"
+// CHECK-NEXT: sycl_vxx.py" "ipexport" "--clang_path" {{.*}} "--target" "xcu200-fsgd2104-2-e"
 // CHECK-NOT: clang
 
 #ifndef __VITIS_KERNEL
