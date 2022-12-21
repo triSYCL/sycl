15a568cbc21885cbb61e44ccff6f9fbb2dc4efd2
Ronan Keryell <rkeryell@xilinx.com>
2022-12-21 02:17:51 -0800

    Remove commented line with AMD FPGA emulation device name selection

--- sycl/test/vitis/disabled/inclusive_devices.cpp
+++ sycl/test/vitis/disabled/inclusive_devices.cpp
@@ -14,13 +14,12 @@ int main() {
   run("Quadro P400", [](auto i, auto a) { a[i] = 2 * a[i]; });
   run("Intel(R) FPGA Emulation Device", [](auto i, auto a) { --a[i]; });
   run("AMD Radeon VII", [](auto i, auto a) { a[i] = a[i] * a[i]; });
-  //run("xilinx_u200_gen3x16_xdma_1_202110_1", [](auto i, auto a) { a[i] += + 3; });
   run("xilinx_u200_gen3x16_xdma_base_1", [](auto i, auto a) { a[i] += + 3; });
   for (auto e : sycl::host_accessor { v })
     std::cout << e << ", ";
   std::cout << std::endl;
 }
 /*
-  To compile according to the available devices, for example with
+  To compile, according to the available devices, for example with
   $DPCPP_HOME/llvm/build/bin/clang++ -std=c++2b -fsycl -fsycl-targets=spir64_x86_64,nvptx64-nvidia-cuda,amdgcn-amd-amdhsa,fpga64_hls_hw,spir64_fpga -Xsycl-target-backend=amdgcn-amd-amdhsa --offload-arch=gfx906 -Xsycl-target-backend=nvptx64-nvidia-cuda --offload-arch=sm_61 inclusive_devices.cpp -o inclusive_devices
 */
