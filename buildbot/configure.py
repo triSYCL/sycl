40a31e3d70cc1262ea3ec39a571792424832a726
Ralender <Tyker1@outlook.com>
2023-07-12 16:20:12 +0200

    Update buildbot/configure.py
    Co-authored-by: Ronan Keryell <ronan@keryell.fr>

--- buildbot/configure.py
+++ buildbot/configure.py
@@ -234,7 +234,7 @@ def main():
                         metavar="BUILD_TYPE", default="Release", help="build type: Debug, Release")
     parser.add_argument("--cuda", action='store_true', help="switch from OpenCL to CUDA")
     parser.add_argument("--xrt", action='store_true', help="switch from OpenCL to XRT")
-    parser.add_argument("--aie", action='store_true', help="add compiler support for building aie application")
+    parser.add_argument("--aie", action='store_true', help="add compiler support for building AMD AIE application")
     parser.add_argument("--hip", action='store_true', help="switch from OpenCL to HIP")
     parser.add_argument("--hip-platform", type=str, choices=['AMD', 'NVIDIA'], default='AMD', help="choose hardware platform for HIP backend")
     parser.add_argument("--host-target", default='X86',
